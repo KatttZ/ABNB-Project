a5a235605fb26e7c40867b7c68a31b918bcabdbb
zhengzhao <zhaozi486@gmail.com>
2024-08-29 18:19:03 -0700

    seed data added

--- backend/db/seeders/20240802212014-demo-spot.js
+++ backend/db/seeders/20240802212014-demo-spot.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
-const {Spot} = require('../models');
+const { Spot } = require("../models");
 
 let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 const spots = [
@@ -18,7 +18,8 @@ const spots = [
     lat: 37.7645358,
     lng: -122.4730327,
     name: "BeachFront Loft",
-    description: "We are Pet Friendly!!!!!!! On BackBeach. Stunning sunsets & spectacular views up the River Teign 2 Dartmoor. Step outside to beach, swimming. Ask to use: Kayak; small boat mooring; firepit & Bar-B-Q. Logburner Oct- thru April. Shared private patio, people watching. Award winning restaurant steps away. Ship Inn, sailing school doors away. Tranquil/vibrant depending on season. Front beach 5 minutes walk. Shaldon Ferry, Arts Quarter, town centre, a few minutes walk. Trains 10 mins walk. Dartmoor National Park under 20 miles. ",
+    description:
+      "We are Pet Friendly!!!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi.",
     price: 150,
   },
   {
@@ -30,7 +31,8 @@ const spots = [
     lat: 40.7645358,
     lng: -120.4730327,
     name: "Glass cottage",
-    description: "We are Pet Friendly!!!!!",
+    description:
+      "We are Pet Friendly!!!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi.",
     price: 160,
   },
   {
@@ -42,7 +44,8 @@ const spots = [
     lat: 33.7645358,
     lng: -34.4730327,
     name: "Cotswolds",
-    description: "We are Pet Friendly!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi. ",
+    description:
+      "We are Pet Friendly!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi. ",
     price: 200,
   },
   {
@@ -54,7 +57,8 @@ const spots = [
     lat: 21.7645358,
     lng: -39.4730327,
     name: "Pinhoti Peak",
-    description: "We are Pet Friendly!!!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi. ",
+    description:
+      "We are Pet Friendly!!!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi. ",
     price: 210,
   },
   {
@@ -66,7 +70,8 @@ const spots = [
     lat: 35.7645358,
     lng: -36.4730327,
     name: "Villa Edge",
-    description: "We are Pet Friendly!!!!!!!Welcome to the brand new Hygge Hideaway!Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi. ",
+    description:
+      "We are Pet Friendly!!!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi.",
     price: 180,
   },
   {
@@ -78,7 +83,8 @@ const spots = [
     lat: 85.7645358,
     lng: -106.4730327,
     name: "Villa Irina",
-    description: "We are Pet Friendly!!!!!!!Welcome to the brand new Hygge Hideaway!Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi. ",
+    description:
+      "We are Pet Friendly!!!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi.",
     price: 190,
   },
   {
@@ -90,7 +96,8 @@ const spots = [
     lat: 71.7645358,
     lng: -106.4730327,
     name: "Luxury Apartment ",
-    description: "We are Pet Friendly!!!!!!!Welcome to the brand new Hygge Hideaway!Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi. ",
+    description:
+      "We are Pet Friendly!!!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi.",
     price: 220,
   },
   {
@@ -102,22 +109,26 @@ const spots = [
     lat: 71.7645358,
     lng: -106.4730327,
     name: "The WonderINN",
-    description: "We are Pet Friendly!!!!!!!Welcome to the brand new Hygge Hideaway!Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi. ",
+    description:
+      "We are Pet Friendly!!!!!!!Place where web developers are createdLorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis posuere magna, vel posuere sem. Vivamus aliquam faucibus tortor, ac posuere nulla vehicula pretium. Suspendisse tincidunt justo et risus dapibus tempor. Donec risus nibh, gravida a nunc eget, ullamcorper mattis nisi.",
     price: 240,
   },
-]
+];
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await Spot.bulkCreate(spots,{validate:true});
+  async up(queryInterface, Sequelize) {
+    await Spot.bulkCreate(spots, { validate: true });
   },
 
-  async down (queryInterface, Sequelize) {
-    options.tableName = 'Spots';
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Spots";
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options,{
-      country:'United States of America'
-    }, {});
-  //  await queryInterface.bulkDelete('Spots',{},{})
-
-  }
+    return queryInterface.bulkDelete(
+      options,
+      {
+        country: "United States of America",
+      },
+      {}
+    );
+    //  await queryInterface.bulkDelete('Spots',{},{})
+  },
 };
